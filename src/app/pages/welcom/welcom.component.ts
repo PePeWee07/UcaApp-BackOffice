c8f6a27fc972f66c49019727326bb71bd05901ca
Pepe <pepewee07@gmail.com>
2024-09-09 08:11:27 -0500

    NEW CHANGES

--- src/app/pages/welcom/welcom.component.ts
+++ src/app/pages/welcom/welcom.component.ts
@@ -1,9 +1,7 @@
-import { UserService } from '../../core/services/user/user.service';
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { AuthService } from '../../core/services/auth/auth.service';
-
 
 @Component({
   selector: 'app-welcom',
@@ -17,23 +15,10 @@ export class WelcomComponent implements OnInit{
   constructor(
     private _route: Router,
     private authService: AuthService,
-    private userService: UserService
     ) {}
 
   ngOnInit(): void {
-    this.test();
   }
-
-  varTest: Object = "...";
-    test(){
-      this.userService.getMethodtest().subscribe( (res) => {
-        console.log("SUCCESS: ", res);
-        this.varTest = res;
-      }, (err) => {
-        console.log("ERROR: ", err);
-        this.varTest = err;
-      });
-    }
 
   logout(){
     this.authService.logout()
